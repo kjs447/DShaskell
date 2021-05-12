c3ee0ae75d12c46fca3f1dbd94f8387189535b34
kjs447 <kjs447@u.sogang.ac.kr>
2021-05-12 17:32:19 +0900

    Define FiniteMap

---
+++ out/production/DShaskell/FiniteMap.hs
@@ -0,0 +1,8 @@
+{-# LANGUAGE MultiParamTypeClasses, FlexibleInstances #-}
+
+module FiniteMap where
+    class Ord key => FiniteMap map key a where
+        empty :: map key a
+        bind :: key -> a -> map key a -> map key a
+        lookup :: key -> map key a -> a
+        
